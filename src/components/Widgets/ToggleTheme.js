5943e8c73dbc3711601fec8e9a37348138fc97d8
maxlech974 <maxime.lechere@gmail.com>
2021-04-05 17:29:16 +0200

    personnalisation du bouton du dark theme avec css native + react feather

--- src/components/Widgets/ToggleTheme.js
+++ src/components/Widgets/ToggleTheme.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Sun, Moon} from 'react-feather';
 
-
+import './toggle.scss';
 
 const ToggleTheme = ({ theme, setTheme }) => {
 
+
+  
   const toggle = () => {
+    
     if(theme === 'light') {
       localStorage.setItem('theme', 'dark');
       setTheme('dark');
@@ -16,9 +20,16 @@ const ToggleTheme = ({ theme, setTheme }) => {
   }
 
   return (
-  <button onClick={toggle}>
-    toggle theme
-  </button>
+
+    <div className="toggle-theme">
+      <div className="toggle-bar" onClick={toggle}>
+        <div className={(theme === 'light') ? "toggle-icon" : "toggle-icon active" }>
+          {(theme === 'light') && (<Sun className="icon" />)}
+          {(theme === 'dark') && (<Moon className="icon" />)}
+        </div>
+      </div>
+    </div>
+  
 
   );
 }
